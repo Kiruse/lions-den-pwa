0623694c536dbbe86c2ef2ca6aa03830109e6793
kiruse <26940742+Kiruse@users.noreply.github.com>
2023-09-28 19:48:58 +0200

    WIP: props overview page

--- pages/daos/:address/props.vue
+++ pages/daos/:address/props.vue
@@ -4,8 +4,12 @@
     DaoNotFound
   else
     DaoHeader(:dao='id')
-    for prop in props
-      DaoProp.surface(:prop='prop')
+    if props.pending
+      Spinner.Props-spinner(color='var(--on-background-highlight)')
+    else
+      NavList.Props-content
+        for prop in props.value
+          NavListItem.Props-item(:to='`#/daos/${address}/props/${prop.id}`')= prop.title
 </template>
 
 <script lang="coffee">
@@ -14,15 +18,30 @@ import { config } from '@/lib/misc'
 import { getProps } from '@/lib/queries/dao-queries'
 import DaoNotFound from '@/views/not-found-dao'
 import DaoHeader from '@/comp/dao/dao-header'
+import DaoProp from '@/comp/dao/dao-prop'
+import NavList from '@/comp/navlist'
+import NavListItem from '@/comp/navlist-item'
+import Spinner from '@/comp/spinner'
 
 export default
-  components: { DaoHeader, DaoNotFound }
+  components: { DaoHeader, DaoNotFound, DaoProp, NavList, NavListItem, Spinner }
   setup: -> { params }
   computed:
     meta: -> Object.values(config.daos).find (dao) => dao.treasury is @params.address
+    address: -> @params.address
     id: -> @meta?.id
   deferred:
     props: ->
       return [] unless @id
-      await getProps @id
+      res = await getProps @id
+      if res.ok
+        res.ok
+      else
+        console.warn 'Failed to load props', res
+        []
 </script>
+
+<style lang="sass">
+.Props-spinner
+  margin: 0 auto
+</style>
